936ced517bb19d8be539ce119f5bbd7716961301
syedusama <usamasyed237@gmail.com>
2024-10-03 15:41:43 +0500

    change the path of store

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import store from "./src/redux/store.jsx";
+import store from "src/redux/store.jsx";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
